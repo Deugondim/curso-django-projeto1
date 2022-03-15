7b25cd0ef46dd579a47a3b3cb5aff7f66c30cf68
Deugondim <deugondim88@gmail.com>
2022-03-15 10:26:22 -0300

    Creating a recipes app e altering te urls

---
+++ recipes/views.py
@@ -0,0 +1,15 @@
+from django.http import HttpResponse
+
+# Create your views here.
+
+
+def home(request):
+    return HttpResponse('HOME ')
+
+
+def sobre(request):
+    return HttpResponse('SOBRE')
+
+
+def contato(request):
+    return HttpResponse('CONTATO')
