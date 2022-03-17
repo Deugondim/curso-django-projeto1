a08568f1152d1eaf5b72ff702baed4288682c4e7
Deugondim <deugondim88@gmail.com>
2022-03-17 10:33:51 -0300

    makemigrations and migrate

--- recipes/models.py
+++ recipes/models.py
@@ -1,9 +1,11 @@
-from distutils.command.upload import upload
-from lib2to3.pytree import convert
-
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+
+
+class Category (models.Model):
+    name = models.CharField(max_length=65)
 
 
 class Recipe (models.Model):
@@ -20,3 +22,9 @@ class Recipe (models.Model):
     updated_at = models.DateField(auto_now=True)
     is_published = models.BooleanField(default=False)
     cover = models.ImageField(upload_to='recipes/covers/%Y/%m/%d')
+    category = models.ForeignKey(
+        Category, on_delete=models.SET_NULL, null=True
+    )
+    author = models.ForeignKey(
+        User, on_delete=models.SET_NULL, null=True
+    )
