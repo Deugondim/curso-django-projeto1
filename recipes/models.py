7b1e58b6a187e67bd60c54369b38f74f6fe6f03a
Deugondim <deugondim88@gmail.com>
2022-03-17 15:00:26 -0300

    creating and ecploring categories

--- recipes/models.py
+++ recipes/models.py
@@ -13,21 +13,26 @@ class Category (models.Model):
 
 class Recipe (models.Model):
     title = models.CharField(max_length=65)
-    descrition = models.CharField(max_length=165)
-    slug = models.SlugField
-    preparation_time = models.IntegerField
+    description = models.CharField(max_length=165)
+    slug = models.SlugField()
+    preparation_time = models.IntegerField()
     preparation_time_unit = models.CharField(max_length=65)
-    servings = models.IntegerField
+    servings = models.IntegerField()
     servings_unit = models.CharField(max_length=65)
-    preparation_steps = models.TextField
-    preparation_steps_is_html = models.BooleanField(default=True)
-    created_at = models.DateField(auto_now_add=True)
-    updated_at = models.DateField(auto_now=True)
+    preparation_steps = models.TextField()
+    preparation_steps_is_html = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
     is_published = models.BooleanField(default=False)
-    cover = models.ImageField(upload_to='recipes/covers/%Y/%m/%d')
+    cover = models.ImageField(
+        upload_to='recipes/covers/%Y/%m/%d/', blank=True, default="")
     category = models.ForeignKey(
-        Category, on_delete=models.SET_NULL, null=True
+        Category, on_delete=models.SET_NULL, null=True, blank=True,
+        default=None,
     )
     author = models.ForeignKey(
         User, on_delete=models.SET_NULL, null=True
     )
+
+    def __str__(self):
+        return self.title
--- recipes/views.py
+++ recipes/views.py
@@ -1,13 +1,17 @@
 from django.shortcuts import render
 from utils.recipes.factory import make_recipe
 
+from .models import Recipe
+
 # Create your views here.
 
 
 def home(request):
+    recipes = Recipe.objects.filter(is_published=True).order_by('-id')
+
     return render(request, 'recipes/pages/home.html', context={
 
-        'recipes': [make_recipe() for _ in range(10)],
+        'recipes': recipes,
     })
 
 
@@ -16,4 +20,14 @@ def recipe(request, id):
 
         'recipe': make_recipe(),
         'is_detail_page': True
+    })
+
+
+def category(request, category_id):
+    recipes = Recipe.objects.filter(
+        category__id=category_id, is_published=True).order_by('-id')
+
+    return render(request, 'recipes/pages/category.html', context={
+
+        'recipes': recipes,
     })
